79d58b7f71e25270d9f96658e5681cdaa27e20a6
Håkon Sønstebø <hakonsonstebo@Hakons-MacBook-Air.local>
2022-03-09 20:18:06 +0100

    tried filter

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,4 +1,3 @@
-from re import T
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import viewsets, mixins, status
@@ -16,13 +15,26 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateMo
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
-    def get_queryset(self):
-        """Return objers for the authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+    def _params_to_int(self, qs):
+        return [int(str_id) for str_id in qs.split(', ')]
 
-    def perform_create(self, serializer):
-        """Create a new tag"""
-        serializer.save(user=self.request.user)
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(',')]
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+        queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user).distinct()
 
 
 class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
